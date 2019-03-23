b040dd5aa7038c395d16da416d2a62f1867e16c8
hussainqasem1995 <48858436+hussainqasem1995@users.noreply.github.com>
2019-03-23 10:20:37 -0700

    Add files via upload

---
+++ fun1.py
@@ -0,0 +1,18 @@
+a="people lived in iraq lived"
+def myfun1(z):
+    print(z)
+myfun1(a)
+def myfun2(x):
+    s=x.split(' ')
+    print(s)
+    return s
+h=myfun2(a)
+def myfun3(c):
+    d=set(c)
+    print(d)
+myfun3(h)
+def myfun4(f):
+    p=f.upper()
+    print(p)
+myfun4(a)
+    
